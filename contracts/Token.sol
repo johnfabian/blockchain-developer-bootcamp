8d588a506bcf3976e1a922efad3a1adb9c0c3bfa
John Fabian <jfabian007@gmail.com>
2022-07-26 01:23:16 +0200

    completed testing module

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,13 +3,29 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+//ERC-20 Standard
+//https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
+
 contract Token {
-    
     //this is a state variable, belongs to entire smart contract
     //adding public will create a getter
-    string public name = "My Token";
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18; //same as eth, 18x 0s after decimal
 
-    
+    //NOTE: million tokens, use math instead of storing the lowest amount of gwei
+    //NOTE: Solidy does not use decimals at all
+    uint256 public totalSupply; //1,000,000. * 10^18
 
-
+    //NOTE: unit256 will default to memory so no need to declare the storage location but is required for string
+    //byte32 may be better to use that string to save on gas
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,21 +1,67 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const getEtherAmountInWei = (ethAmount) => {
+    //use https://eth-converter.com/ to get the value in wei of 1,000,000 tokens
+    //or use ethers.js    
+    return ethers.utils.parseUnits(ethAmount.toString(), 'ether');
+}
+
+
 describe("Token", () => {
 
-    it("Has a name", async () => {
-       
+    let token;
+    const name = "Dapp University";
+    const symbol = "DAPP";
+    const totalSupply = 1000000;
+    const decimals = "18"
+
+    //setup test, runs before each test below
+    beforeEach(async () => {
         //get contract
         const Token = await ethers.getContractFactory("Token");
 
-        //deploy
-        let token = await Token.deploy();
-
-        //read the token from block chain
-        const name = await token.name();        
-
-        //check that name is correct
-        expect(name).to.equal("My Token");
+        //deploy, NOTE :This will run the constructor
+        token = await Token.deploy(name, symbol, totalSupply.toString());
 
     });
+
+
+    describe("Deployment", () => {
+
+        //name
+        it("Has correct name", async () => {
+            //check that name is correct
+            expect(await token.name()).to.equal(name);
+
+        });
+
+        // symbol
+        it("Has correct symbol", async () => {
+            //check that name is correct
+            expect(await token.symbol()).to.equal(symbol);
+        });
+
+        //decimals
+        it("Has correct decimals", async () => {
+            //check that name is correct
+            expect(await token.decimals()).to.equal(decimals);
+        });
+
+        //total supply
+        it("Has correct totalSupply", async () => {
+
+            //check that name is correct
+            expect(await token.totalSupply()).to.equal(getEtherAmountInWei(totalSupply.toString()));
+        });
+
+
+    })
+
+
+
+
+
+
+
 });
